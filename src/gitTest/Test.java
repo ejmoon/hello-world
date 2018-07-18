36c695dfe688f4c71b8ed5db267ea9402a9cfcdc
JuJu <JuJu@JuJu-gram>
2018-07-19 04:15:46 +0900

    second test

--- src/gitTest/Test.java
+++ src/gitTest/Test.java
@@ -3,8 +3,8 @@ package gitTest;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		
+		System.out.println("world hello");
+		System.out.println("hi world");
 	}
 
 }
